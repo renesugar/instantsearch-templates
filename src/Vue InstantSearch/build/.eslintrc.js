0677db434ad5fad718c4ae504fd3a51a5ab2842e
vvo <vincent.voyer@algolia.com>
2018-02-09 17:35:59 +0100

    feat(Vue): add Vue InstantSearch template
    using vue-cli webpack template
    
    fixes #7

---
+++ src/Vue InstantSearch/build/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: '../.eslintrc.js',
+  rules: {
+    'import/no-commonjs': 'off',
+    'no-console': 'off',
+    'no-process-exit': 'off',
+    'no-param-reassign': 'off',
+    'guard-for-in': 'off'
+  }
+};
